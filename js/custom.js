5c8299cfa3d1e7a44458ee8ba455f043ec31b3c2
root <root@vps32915.lws-hosting.com>
2017-05-23 23:31:14 +0200

    First commit...

---
+++ js/custom.js
@@ -0,0 +1,115 @@
+$(document).ready(function(){
+    
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+		BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+		iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+		Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+		any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+	
+	if ($(".featured-jobs-tab > div.attachment > div.super-featured-jobs").children().length == 0){
+		$(".featured-jobs-tab > div.attachment").hide();
+	}
+	
+	if($("a.apply-btn").length){
+		$("div.warning-message").show();
+	}else{
+		$("div.warning-message").hide();
+	}
+	//Employer social media buttons
+	$(".employer-fcbk").text("");
+	$(".employer-twitter").text("");
+	$(".employer-linkedin").text("");
+	$("div.employer-social-media > div").addClass('col-lg-1 col-md-1 col-sm-1 col-xs-1');
+
+	//Badges jobs count
+	$(".view-all-jobs").parent(".content").parent(".block-system").parent(".region-content").addClass('jobs-tabs');
+	
+	var featured = '<span class="badge">' + $("#featuredid").text() + '</span>';
+	var alljobs = '<span class="badge">' + $("#alljobsid").text() + '</span>';
+	var publics = '<span class="badge">' + $("#publicid").text() + '</span>';
+	var tenders = '<span class="badge">' + $("#tendersid").text() + '</span>';
+	var interns = '<span class="badge">' + $("#internid").text() + '</span>';
+	var others = '<span class="badge">' + $("#othersid").text() + '</span>';
+	
+	$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(1) > a").append(featured);
+	$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(2) > a").append(alljobs);
+	$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(3) > a").append(tenders);
+	$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(4) > a").append(interns);
+	$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(5) > a").append(others);
+	$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(6) > a").append(publics);
+	
+	if($("#featuredid").text() == "0"){
+		$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(1)").css('display', 'none');
+	}
+	if($("#alljobsid").text() == "0"){
+		$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(2)").css('display', 'none');
+	}
+	if($("#tendersid").text() == "0"){
+		$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(3)").css('display', 'none');
+	}
+	if($("#internid").text() == "0"){
+		$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(4)").css('display', 'none');
+	}
+	if($("#othersid").text() == "0"){
+		$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(5)").css('display', 'none');
+	}
+	if($("#publicid").text() == "0"){
+		$(".jobs-tabs > div.tabs > ul.nav-tabs > li:nth-child(6)").css('display', 'none');
+	}
+	//End
+	$(".simplenews-subscribe > div > div.form-type-textfield > label").css('display', 'none');
+	
+	$(".news-view-page > div.view-content > div > ul.media-list > li.media > div.pull-left > a > img").addClass('img-thumbnail');
+	$("#edit-realname").addClass('form-control');
+	$("#edit-combine-wrapper").addClass('input-group');
+	$("#edit-combine").addClass('form-control');
+	$("#edit-submit-all-jobs").removeClass('btn-primary').addClass('btn-default');
+	$("#edit-field-employer-und-add-more").removeClass('btn-info').addClass('btn-default');
+	$("#edit-field-employer-und-add-more").text('Select employer');
+	$(".custom-apply-btn > h3 > span > a").addClass('btn btn-link');
+	$("#btn-block-afb-1 > button").addClass('btn-block').text("Add employer");
+	$("#ajax-submit-random-1146499521").removeClass('ajax-processed');
+	$("#edit-field-employer").append("<div><a href='/node/add/employer'>Can't find your company name? Click here.</a></div>");
+	
+	$(".captcha > .control-group > .control-label").html("Random question <span title='This field is required.' class='form-required'>*</span>");
+	if(isMobile.any()){
+		$("#back-top").css('bottom', '50px');
+		$("#section-footer").css('padding-bottom', '60px');
+		$("div.horizontal-banner").css('display', 'none');
+		$("#section-tweets").css('display', 'none');
+	}else{
+		$("#edit-submit").removeClass('btn-primary').addClass('btn-default');
+	}
+	
+	$(".simplenews-unsubscribe > div > .btn.form-submit, .simplenews-subscribe > div > .btn.form-submit").addClass('btn-default btn-block');	
+	$("button[value='Upload']").hide();
+	$(".term-level-element-table > thead > tr > th:nth-child(2)").text("None");
+	$(".term-level-element-table > tbody > tr.term-level-element-table-row > td:nth-child(1)").css('width', '25%');
+	$(".term-level-element-table > tbody > tr.term-level-element-table-row > td:nth-child(2)").css('width', '15%');
+	$(".term-level-element-table > tbody > tr.term-level-element-table-row > td:nth-child(3)").css('width', '15%');
+	$(".term-level-element-table > tbody > tr.term-level-element-table-row > td:nth-child(4)").css('width', '15%');
+	$(".term-level-element-table > tbody > tr.term-level-element-table-row > td:nth-child(5)").css('width', '15%');
+	$(".term-level-element-table > tbody > tr.term-level-element-table-row > td:nth-child(6)").css('width', '15%');
+	$(".term-level-element-table > tbody > tr.term-level-element-table-row > td:nth-child(7)").hide();
+	$("#views-exposed-form-all-jobs-jobs-search-page > div > div.views-exposed-form > div.views-exposed-widgets > #edit-combine-wrapper > div.views-widget > div.form-item-combine > div.controls > #edit-combine").attr('placeholder', 'Job title or company name e.g. manager, tigo, ...');
+	$("#edit-realname").attr('placeholder', 'Names');
+	$("#edit-field-employer-und-0-target-id").attr('placeholder', 'Search company name');
+	$(".simplenews-subscribe > div > div.form-item-mail > div.controls > input").attr('placeholder', 'Email');
+	$("div.node-news.view-mode-full > div > div.field-name-field-news-photo > div.field-items > div.field-item > img").addClass('thumbnail');
+	$("#job-node-form > div > div#edit-actions > #edit-submit").text("Submit");
+});
---
+++ template.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Custom Galeria Template settings
+ */
+ 
+/**
+ * page alter
+ */
+function consilium_page_alter(&$vars) {
+	
+	// Add custom varibales for scss
+	$theme = superhero_get_theme();
+	$theme->custom['header_height'] = theme_get_setting('superhero_header_height');
+	$theme->custom['header_fixed_height'] = theme_get_setting('superhero_header_fixed_height');
+  
+	// Remove content from front page
+	/*
+	if (drupal_is_front_page()) {
+		unset($vars['data']['content']);
+	}*/
+}
+
+/**
+ * Implement hook_breadcrumd
+ */
+function consilium_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    //$output = '<span class="showHere left">' . t('You are here') . '</span>';
+
+    $output = '<div class="breadcrumb"><span class="showHere left">' . t('You are here:') . '</span> ' . implode(' <span class="fa fa-angle-right"></span> ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
+//Custom codes
+
+function consilium_node_preview($variables) {
+  $node = $variables['node'];
+  $elements = node_view($node, 'full');
+  $full = drupal_render($elements);
+  $output = '<div class="preview">';
+  $output .= '<h3 class="post-preview" >' . t('Preview of your posting') . '</h3>';
+  $output .= $full;
+  $output .= "</div>\n";
+  return $output;
+}
+
+// Remove Query Strings from CSS filenames (CacheBuster)
+function consilium_process_html(&$variables) {
+  $variables['styles'] = preg_replace('/\.css\?.*"/','.css"', $variables['styles']);
+}
